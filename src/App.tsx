9ae629597dc18de527decb174afbc5ae1dce89b3
Iggy Villamar <iggy@ivstudio.com>
2021-08-31 21:56:17 -0400

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
+import { TrChelsea, TrEric, TrSamantha, TrTorsten } from 'tyger-avatar';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const avatars = [
+	{ component: <TrChelsea />, name: 'TrChelsea' },
+	{ component: <TrEric />, name: 'TrEric' },
+	{ component: <TrSamantha />, name: 'TrSamantha' },
+	{ component: <TrTorsten />, name: 'TrTorsten' },
+];
+
+const App = () => {
+	return (
+		<div className="container">
+			{avatars.map((item: any) => {
+				const avatar = React.cloneElement(item.component, {
+					width: 140,
+				});
+
+				return (
+					<div key={item.name}>
+						<div className="avatar-item">{avatar}</div>
+						<p>{item.name}</p>
+					</div>
+				);
+			})}
+		</div>
+	);
+};
 
 export default App;
