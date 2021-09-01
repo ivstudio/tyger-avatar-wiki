c6acc60bc7927c1ba504c2afebb697ef832eddff
Iggy Villamar <iggy@ivstudio.com>
2021-09-01 08:10:07 -0400

    Add new avatars

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,55 @@
 import React from 'react';
-import { TrChelsea, TrEric, TrSamantha, TrTorsten } from 'tyger-avatar';
+import {
+	TrChelsea,
+	TrEric,
+	TrSamantha,
+	TrTorsten,
+	TrIggy,
+	TrFranklin,
+	TrImran,
+	TrMaria,
+	TrShamila,
+	TrRachel,
+} from 'tyger-avatar';
 import './App.css';
 
 const avatars = [
 	{ component: <TrChelsea />, name: 'TrChelsea' },
 	{ component: <TrEric />, name: 'TrEric' },
 	{ component: <TrSamantha />, name: 'TrSamantha' },
+	{ component: <TrRachel />, name: 'TrRachel' },
+	{ component: <TrShamila />, name: 'TrShamila' },
+	{ component: <TrIggy />, name: 'TrIggy' },
+	{ component: <TrFranklin />, name: 'TrFranklin' },
+	{ component: <TrImran />, name: 'TrImran' },
+	{ component: <TrMaria />, name: 'TrMaria' },
 	{ component: <TrTorsten />, name: 'TrTorsten' },
 ];
 
+interface IAvatar {
+	component: JSX.Element;
+	name: string;
+}
+
 const App = () => {
+	const handleClick = (name: string) => {
+		console.log(`${name} was clicked`);
+	};
+
 	return (
 		<div className="container">
-			{avatars.map((item: any) => {
-				const avatar = React.cloneElement(item.component, {
+			{avatars.map(({ name, component }: IAvatar) => {
+				const avatar = React.cloneElement(component, {
 					width: 140,
+					title: name,
+					id: `tyger${name}`,
+					onClick: () => handleClick(name),
 				});
 
 				return (
-					<div key={item.name}>
+					<div key={name}>
 						<div className="avatar-item">{avatar}</div>
-						<p>{item.name}</p>
+						<p>{name}</p>
 					</div>
 				);
 			})}
